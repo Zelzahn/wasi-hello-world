59e7b2b55b87fb7d1199387661014a686766fe34
Zelzahn <friedrich@friedrichvdb.be>
2024-02-23 09:49:56 +0100

    switch to sync

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,34 +1,24 @@
 use wasmtime::component::{Component, Linker};
 use wasmtime::{Config, Engine, Store};
-use wasmtime_wasi::preview2::{ResourceTable, WasiCtx, WasiView, WasiCtxBuilder};
-use futures::executor::block_on;
-use async_trait::async_trait;
+use wasmtime_wasi::preview2::{ResourceTable, WasiCtx, WasiCtxBuilder, WasiView};
 
-// Imports will be async functions through #[async_trait] and exports
-// are also invoked as async functions. Requires `Config::async_support`
-// to be `true`.
-wasmtime::component::bindgen!({
-    path: "../wit",
-    world: "hello-world",
-    async: true,    // wasmtime-wasi currently only has an async implementation
-});
+wasmtime::component::bindgen!("hello-world" in "../wit/my-component.wit");
 
 struct MyState {
     name: String,
     table: ResourceTable,
-    wasi: WasiCtx
+    wasi: WasiCtx,
 }
 
-#[async_trait]
 impl HelloWorldImports for MyState {
-    async fn name(&mut self) -> wasmtime::Result<String> {
+    fn name(&mut self) -> wasmtime::Result<String> {
         Ok(self.name.clone())
     }
 }
 
 // Needed for wasmtime_wasi::preview2
 impl WasiView for MyState {
-    fn table(&self) -> &ResourceTable{
+    fn table(&self) -> &ResourceTable {
         &self.table
     }
     fn table_mut(&mut self) -> &mut ResourceTable {
@@ -45,13 +35,14 @@ impl WasiView for MyState {
 fn main() -> wasmtime::Result<()> {
     // Configure an `Engine` and compile the `Component` that is being run for
     // the application.
-    // Async support is needed for wasmtime linker
     let mut config = Config::new();
-    config.wasm_component_model(true)
-          .async_support(true);
+    config.wasm_component_model(true);
     let engine = Engine::new(&config)?;
     // let component = Component::from_file(&engine, "../guest/guest.component.wasm")?;
-    let component = Component::from_file(&engine, "../guest-cargo/target/wasm32-wasi/debug/guest_cargo.wasm")?;
+    let component = Component::from_file(
+        &engine,
+        "../guest-cargo/target/wasm32-wasi/debug/guest_cargo.wasm",
+    )?;
 
     // Instantiation of bindings always happens through a `Linker`.
     // Configuration of the linker is done through a generated `add_to_linker`
@@ -64,7 +55,7 @@ fn main() -> wasmtime::Result<()> {
     let mut linker = Linker::new(&engine);
 
     // Bind wasi commmand world
-    wasmtime_wasi::preview2::command::add_to_linker(&mut linker)?;
+    wasmtime_wasi::preview2::command::sync::add_to_linker(&mut linker)?;
     // Binding host
     HelloWorld::add_to_linker(&mut linker, |state: &mut MyState| state)?;
 
@@ -76,30 +67,26 @@ fn main() -> wasmtime::Result<()> {
     let table = ResourceTable::new();
 
     let wasi = WasiCtxBuilder::new()
-            // .inherit_stderr()
-            // .inherit_stdin()
-            .inherit_stdout()
-            .inherit_stdio()
-            .build();
+        // .inherit_stderr()
+        // .inherit_stdin()
+        .inherit_stdout()
+        .inherit_stdio()
+        .build();
 
     let mut store = Store::new(
         &engine,
         MyState {
             name: "me".to_string(),
             table,
-            wasi
+            wasi,
         },
     );
 
-    block_on(async {
-        let (bindings, _) = HelloWorld::instantiate_async(&mut store, &component, &linker).await?;
+    let (bindings, _) = HelloWorld::instantiate(&mut store, &component, &linker)?;
 
-        // Here our `greet` function doesn't take any parameters for the component,
-        // but in the Wasmtime embedding API the first argument is always a `Store`.
-        bindings.call_greet(&mut store).await?;
+    // Here our `greet` function doesn't take any parameters for the component,
+    // but in the Wasmtime embedding API the first argument is always a `Store`.
+    bindings.call_greet(&mut store)?;
 
-        Ok::<(), anyhow::Error>(())
-    })?;
-
-    Ok(())
+    Ok::<(), anyhow::Error>(())
 }
